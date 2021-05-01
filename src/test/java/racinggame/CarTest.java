0648cb001dbb7dfaaf926080df7af46a62861d5c
mkhwang <orange2652@gmail.com>
2021-05-02 03:40:07 +0900

    test : add move position test

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -2,6 +2,8 @@ package racinggame;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,5 +20,17 @@ public class CarTest {
         Car car = new Car("TEST");
         assertThat(car.getCarName()).isEqualTo("TEST");
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest(name = "{index} value = {0}")
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9})
+    void moveTest(int value) {
+        Car car = new Car("명권차");
+        car.move(value);
+        if(value >= 4){
+            assertThat(car.getPosition()).isEqualTo(1);
+        }else {
+            assertThat(car.getPosition()).isEqualTo(0);
+        }
     }
 }
