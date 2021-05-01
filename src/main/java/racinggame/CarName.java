4f23688fdded7b7d6f280d5fde596154e0b0abeb
mkhwang <orange2652@gmail.com>
2021-05-02 02:48:39 +0900

    refactor: change wraped method
    carname method getName() to name()
    add CarName.class create

--- src/main/java/racinggame/CarName.java
+++ src/main/java/racinggame/CarName.java
@@ -16,13 +16,17 @@ public class CarName {
         this.name = carName;
     }
 
+    public static CarName create(String carName) {
+        return new CarName(carName);
+    }
+
     private void validNameLength(String carName) {
         if(carName.length() > MAX_NAME_LENGTH){
             throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
         }
     }
 
-    public String getName() {
+    public String name() {
         return this.name;
     }
 }
