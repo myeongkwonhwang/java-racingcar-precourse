b21e3838e87863e02bc5e67de61805660489b941
mkhwang <orange2652@gmail.com>
2021-05-02 02:37:33 +0900

    test : input carnames and round test

--- src/test/java/racinggame/RacingGameTest.java
+++ src/test/java/racinggame/RacingGameTest.java
@@ -1,5 +1,6 @@
 package racinggame;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,8 +18,18 @@ public class RacingGameTest {
     @Test
     @DisplayName(value = "입력한 자동차 이름만큼 자동차 생성")
     void inputCarNames_ReturnCarsLength() {
-        RacingGame racingGame = new RacingGame(Arrays.asList("hwang", "myeong", "kwon"));
+        RacingGame racingGame = new RacingGame(Arrays.asList("hwang", "mk", "kwon"), 5);
         int joinedCarCount = racingGame.getJoinedCarCount();
         assertThat(3).isEqualTo(joinedCarCount);
+    }
+
+    @Test
+    @DisplayName(value = "입력한 자동차 이름만큼 자동차 생성과 라운드 테스트")
+    void inputCarNamesAndRoundTest() {
+        RacingGame racingGame = new RacingGame(Arrays.asList("hwang", "mk", "kwon"), 5);
+        int joinedCarCount = racingGame.getJoinedCarCount();
+        int racingRound = racingGame.getRacingRound();
+        assertThat(3).isEqualTo(joinedCarCount);
+        assertThat(5).isEqualTo(racingRound);
     }
 }
