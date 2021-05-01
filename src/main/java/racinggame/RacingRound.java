4ea50a291f9ac1341f86eec3d1ece605ded926c9
mkhwang <orange2652@gmail.com>
2021-05-02 02:49:26 +0900

    feat : wraped RacingRound

---
+++ src/main/java/racinggame/RacingRound.java
@@ -0,0 +1,19 @@
+package racinggame;
+
+/**
+ * Created by mkhwang on 2021/05/02
+ * Email : orange2652@gmail.com
+ * Github : https://github.com/myeongkwonhwang
+ */
+public class RacingRound {
+
+    private final int round;
+
+    public RacingRound(int round) {
+        this.round = round;
+    }
+
+    public int round() {
+        return this.round;
+    }
+}
