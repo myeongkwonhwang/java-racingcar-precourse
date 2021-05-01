0d621f836b5fcbb9573687ec091b7084a98d9854
mkhwang <orange2652@gmail.com>
2021-05-02 02:17:47 +0900

    feat : wraped carname / length valid

---
+++ src/main/java/racinggame/CarName.java
@@ -0,0 +1,28 @@
+package racinggame;
+
+/**
+ * Created by mkhwang on 2021/05/02
+ * Email : orange2652@gmail.com
+ * Github : https://github.com/myeongkwonhwang
+ */
+public class CarName {
+
+    public static final int MAX_NAME_LENGTH = 5;
+
+    private String name;
+
+    public CarName(String carName) {
+        validNameLength(carName);
+        this.name = carName;
+    }
+
+    private void validNameLength(String carName) {
+        if(carName.length() > MAX_NAME_LENGTH){
+            throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
